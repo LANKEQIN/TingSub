bc1f4c6eaa7e59e22a2f21de532e7f364b5634da
ShindoAmane <18376668690@163.com>
2025-09-29 10:23:11 +0800

    1111

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { TamaguiProvider } from 'tamagui';
 import tamaguiConfig from './tamagui.config';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Home, BarChart3, Bell, User } from '@tamagui/lucide-icons';
 import HomeScreen from './screens/HomeScreen';
 import StatisticsScreen from './screens/StatisticsScreen';
 import NotificationsScreen from './screens/NotificationsScreen';
@@ -16,13 +17,48 @@ export default function App() {
     <TamaguiProvider config={tamaguiConfig}>
       <NavigationContainer>
         <Tab.Navigator
-          screenOptions={{
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+              let IconComponent;
+              
+              if (route.name === 'Home') {
+                IconComponent = Home;
+              } else if (route.name === 'Statistics') {
+                IconComponent = BarChart3;
+              } else if (route.name === 'Notifications') {
+                IconComponent = Bell;
+              } else if (route.name === 'Profile') {
+                IconComponent = User;
+              }
+              
+              return <IconComponent size={size} color={color} />;
+            },
+            tabBarActiveTintColor: '#007AFF',
+            tabBarInactiveTintColor: '#8E8E93',
             tabBarStyle: {
-              paddingBottom: 5,
-              paddingTop: 5,
-              height: 60,
-            }
-          }}
+              backgroundColor: '#FFFFFF',
+              borderTopWidth: 0,
+              elevation: 20,
+              shadowColor: '#000',
+              shadowOffset: {
+                width: 0,
+                height: -2,
+              },
+              shadowOpacity: 0.1,
+              shadowRadius: 10,
+              paddingBottom: 8,
+              paddingTop: 8,
+              height: 70,
+              borderTopLeftRadius: 20,
+              borderTopRightRadius: 20,
+            },
+            tabBarLabelStyle: {
+              fontSize: 12,
+              fontWeight: '500',
+              marginTop: 4,
+            },
+            headerShown: false,
+          })}
         >
           <Tab.Screen 
             name="Home" 
