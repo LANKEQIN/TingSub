84a67ac76a3e95b59ed37afcbc4dfe9c160bcfae
ShindoAmane <18376668690@163.com>
2026-01-17 15:20:30 +0800

    添加 Tingsub 项目

---
+++ Tingsub/types/env.d.ts
@@ -0,0 +1,8 @@
+declare module '@env' {
+  export const APP_ENV: string;
+  export const APP_NAME: string;
+  export const APP_VERSION: string;
+  export const API_BASE_URL: string;
+  export const DEBUG: string;
+  export const LOG_LEVEL: string;
+}
